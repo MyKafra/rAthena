7d2a4f0b97d0e474c36a3863dab1656917da8bee
MrPostman <MrPostman@windowslive.com>
2016-02-03 22:15:39 +0700

    ระบบ fakeuser(2)

--- src/map/chrif.c
+++ src/map/chrif.c
@@ -1826,7 +1826,7 @@ int chrif_parse(int fd) {
 int send_usercount_tochar(int tid, unsigned int tick, int id, intptr_t data) {
 	chrif_check(-1);
 
-	WFIFOHEAD(char_fd,4);
+	WFIFOHEAD(char_fd,6);
 	WFIFOW(char_fd,0) = 0x2afe;
 	WFIFOW(char_fd,2) = map_usercount();
 	WFIFOW(char_fd, 4) = fake_users;
